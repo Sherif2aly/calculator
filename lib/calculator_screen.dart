703d924fc83a92cd646c45d848e818325aea4816
Sherif2aly <sheriefmahmoud384@gmail.com>
2023-10-08 15:32:48 +0300

    create calculator screen

---
+++ lib/calculator_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String userInput = '';
+  String result = '0';
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 2.79,
+              child: resultWidget(),
+            ),
+            // const Expanded(
+            //   child: BottonWidget(),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
